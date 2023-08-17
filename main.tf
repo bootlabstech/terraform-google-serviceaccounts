52e10aaafa94f6c87c0fdcde85a89b886e92ced4
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-17 19:39:36 +0530

    rem si

--- main.tf
+++ main.tf
@@ -9,19 +9,13 @@ resource "google_service_account_key" "service_account_keys" {
   count              = length(var.service_accounts)
   service_account_id = google_service_account.service_accounts[count.index].name
 }
-resource "google_project_service_identity" "sm_sa" {
-  provider = google-beta
-
-  project = var.project_id
-  service = "secretmanager.googleapis.com"
-}
 resource "google_secret_manager_secret" "service_account_secrets" {
   count     = length(var.service_accounts)
   project   = var.project_id
   secret_id = "service-account-secret-${count.index}"
   replication {
-    user_managed {
-      replicas {
+    user_managed{
+      replicas{
         location = var.location
         customer_managed_encryption {
           kms_key_name = var.cmek
@@ -32,7 +26,6 @@ resource "google_secret_manager_secret" "service_account_secrets" {
   lifecycle {
     ignore_changes = [labels]
   }
-  depends_on = [google_project_service_identity.sm_sa]
 }
 
 resource "google_secret_manager_secret_version" "service_account_secret_versions" {
@@ -41,3 +34,4 @@ resource "google_secret_manager_secret_version" "service_account_secret_versions
 
   secret_data = google_service_account_key.service_account_keys[count.index].private_key
 }
+
